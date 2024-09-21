ea47b3a69d6ad36d703d962bc2ea11435178ba31
Rakesh Sunda <rswami@rswami.in>
2024-09-22 05:00:26 +0530

    Update index.js
    new start

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /*! standard. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 import { StandardEngine } from 'standard-engine'
 import options from './lib/options.js'
-
 export default new StandardEngine(options)
